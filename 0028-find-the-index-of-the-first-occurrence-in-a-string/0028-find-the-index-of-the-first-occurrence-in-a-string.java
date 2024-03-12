2ea577370445ec8e52f82af76cae12bcf177d7bb
Venu Gopal Dummu <venugopal.dummu@gmail.com>
2024-03-12 11:58:20 -0500

    Time: 0 ms (100.00%), Space: 41.3 MB (60.47%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        for (int i = 0; i < (haystack.length() - needle.length())+1; i++) {
+			
+	        if(haystack.charAt(i) == needle.charAt(0)   && haystack.substring(i, i+needle.length()).equals(needle)) {
+				return i;
+			}
+		}
+		return -1;
+    }
+}

d3854bc4ca56d930c030d9a91da0ceb440bc92f6
Venu Gopal Dummu <venugopal.dummu@gmail.com>
2024-03-17 15:34:20 -0500

    Time: 0 ms (100.00%), Space: 44.3 MB (48.16%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode i = head;
+		ListNode j = head;
+		
+		while(i != null && j!=null && j.next!=null) {
+			
+			i = i.next;
+			j =  j.next.next;
+			
+			if(i ==  j) {
+				return true;
+			}
+		}
+		
+		return false;
+    }
+}

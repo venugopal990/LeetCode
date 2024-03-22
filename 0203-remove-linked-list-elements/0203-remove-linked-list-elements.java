7368c2e2a012e03423c00592170f8265741513ad
Venu Gopal Dummu <venugopal.dummu@gmail.com>
2024-03-21 20:02:54 -0500

    Time: 1 ms (92.80%), Space: 45.6 MB (37.06%) - LeetHub

---
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+
+        if(head == null)
+            return head;
+
+        ListNode result = new  ListNode(0);
+        result.next  = head;
+
+
+        ListNode curr = result;
+        while(curr.next != null){
+            if(curr.next.val == val){
+                curr.next = curr.next.next;
+            }else{
+                curr = curr.next;
+            }
+        
+        }
+
+        return result.next;
+    }
+}

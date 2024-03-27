50142fd71724bb30b6b8bbfee09b1ac8be985a58
Venu Gopal Dummu <venugopal.dummu@gmail.com>
2024-03-27 13:26:44 -0500

    Time: 0 ms (100.00%), Space: 40.7 MB (87.91%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+
+        ListNode i = head;
+        ListNode j = head;
+
+        while(j!=null && j.next!=null){
+            i=i.next;
+            j=j.next.next;
+        }
+
+
+
+        return i;
+
+        
+    }
+}

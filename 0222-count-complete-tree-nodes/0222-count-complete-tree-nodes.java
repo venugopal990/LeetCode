3bda22bfbd20f3720ec8146a7bde7dc3d456f679
Venu Gopal Dummu <venugopal.dummu@gmail.com>
2024-03-28 23:32:17 -0500

    Time: 0 ms (100.00%), Space: 47.8 MB (26.89%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+
+        if(root == null){
+            return 0;
+        }
+
+        int leftTree = countNodes(root.left);
+        int rightTree = countNodes(root.right);
+
+        return leftTree+rightTree+1;
+
+        
+    }
+}

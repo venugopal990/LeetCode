2e3c47455f5d25376bf8d0665eb774bd4ab71613
Venu Gopal Dummu <venugopal.dummu@gmail.com>
2024-04-06 02:38:12 -0500

    Time: 12 ms (95.90%), Space: 43.2 MB (38.22%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -2,6 +2,7 @@ class Solution {
     public int threeSumClosest(int[] nums, int target) {
 
         int closestSum = nums[0] + nums[1] + nums[2];
+        int minValue = Integer.MAX_VALUE;
         Arrays.sort(nums);
 
         for(int i=0;i<nums.length-2;i++){
@@ -9,13 +10,19 @@ class Solution {
             int k=nums.length-1;
             while(j<k){
                 int sum =nums[i]+nums[j]+nums[k];
-                if(Math.abs(target - sum) < Math.abs(target - closestSum)){
-                    closestSum = sum;
+                if(sum == target){
+                    return target;
                 }
                 if(sum>target){
                     k--;
                 }else{
                     j++;
+                }
+
+
+                if(Math.abs(target - sum) < minValue){
+                    closestSum = sum;
+                    minValue = Math.abs(target - sum);
                 }
             }
         }

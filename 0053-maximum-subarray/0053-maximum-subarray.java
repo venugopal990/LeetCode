3177ee530e4108f93bc8d5e03b5131a0a81ebc05
Venu Gopal Dummu <venugopal.dummu@gmail.com>
2024-04-16 12:53:05 -0500

    Time: 1 ms (99.42%), Space: 57 MB (40.72%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+
+
+        int maxSum = nums[0];
+        int sum = 0;
+
+        for(int i=0;i<nums.length;i++){
+                sum=nums[i]+sum;
+                maxSum = Math.max(sum,maxSum);
+                 if(sum<0){
+                    sum = 0;
+                }
+        }
+        return maxSum; 
+    }
+}

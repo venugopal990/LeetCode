e2e06d990b5f12887befa106a5b63dd3e6f4e8a2
Venu Gopal Dummu <venugopal.dummu@gmail.com>
2024-04-15 22:51:48 -0500

    Time: 2 ms (79.62%), Space: 52.6 MB (64.41%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,21 +1,25 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int majority=nums[0];
-		int vote=1;
-		
-		for (int i = 1; i < nums.length; i++) {
-			
-			if(majority == nums[i]) {
-				vote++;
-			}else {
-				vote--;
-				if(vote == 0) {
-					majority = nums[i];
-					vote++;
-				}
-			}
-		
-		}
-		return majority;
+
+        int majority = nums[0];
+        int vote = 1;
+
+        for(int i=0;i<nums.length;i++){
+
+                if(nums[i] == majority){
+                    vote++;
+                }else{
+                    vote--;
+                }
+
+                if(vote == 0){
+                    majority = nums[i];
+                    vote = 1;
+                }
+        }
+
+
+        return majority;
+        
     }
 }

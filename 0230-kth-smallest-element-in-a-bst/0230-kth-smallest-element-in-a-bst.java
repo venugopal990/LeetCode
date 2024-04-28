cd5c0c41788dadc326ca4b195782c5009e799d0e
Venu Gopal Dummu <venugopal.dummu@gmail.com>
2024-04-28 11:46:28 -0500

    Time: 0 ms (100.00%), Space: 44 MB (88.26%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -16,36 +16,30 @@
 class Solution {
 
     int count = 0;
+    int min  = 0;
 
     public int kthSmallest(TreeNode root, int k) {
 
-        Queue<Integer> queue = new LinkedList<>();
         count = k;
-        inOrder(root, queue);
-
-        int min = Integer.MAX_VALUE;
-        while (!queue.isEmpty() && k > 0) {
-            min = queue.poll();
-            k--;
-        }
-
+        inOrder(root);
         return min;
 
     }
 
-    private void inOrder(TreeNode root, Queue<Integer> queue) {
+    private void inOrder(TreeNode root) {
 
         if (root == null) {
             return;
         }
 
+        inOrder(root.left);
         if (count > 0) {
-            inOrder(root.left, queue);
-            queue.offer(root.val);
+            min = root.val;
             count--;
-            inOrder(root.right, queue);
         } else {
             return;
         }
+        inOrder(root.right);
+
     }
 }

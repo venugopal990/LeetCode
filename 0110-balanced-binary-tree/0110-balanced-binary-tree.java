ebe8ae64f8004e17bed297a8d97f6383f30dff3f
Venu Gopal Dummu <venugopal.dummu@gmail.com>
2024-05-13 17:03:14 -0500

    Time: 0 ms (100.00%), Space: 44.6 MB (12.38%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -15,23 +15,18 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-
-        if(root == null){
-            return true;
-        }
-
-        if(Math.abs(height(root.left) - height(root.right))>1) return false;
-        return isBalanced(root.left) &&  isBalanced(root.right);
-        
+        return height(root)!=-1;
     }
 
-    public int height(TreeNode root) {
 
+    private int height(TreeNode root){
         if(root == null){
             return 0;
         }
-        return  Math.max(height(root.left),height(root.right))+1;
+        int left = height(root.left);
+        int right = height(root.right);
+        if(left == -1 || right == -1) return -1;
+        if(Math.abs(left-right) > 1) return -1;
+        return Math.max(left,right)+1;
     }
-
-
 }

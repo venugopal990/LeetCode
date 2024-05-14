60249bb8668216e17410a8fc88e8645de9d6c821
Venu Gopal Dummu <venugopal.dummu@gmail.com>
2024-05-14 16:35:35 -0500

    Time: 6 ms (7.88%), Space: 43.2 MB (7.05%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -13,68 +13,63 @@
  *     }
  * }
  */
- class Tuple{
-    TreeNode node;
-    int row;
-    int col;
-    public Tuple(TreeNode node,int row,int col){
-        this.node = node;
-        this.row = row;
-        this.col = col;
-
-    }
- }
 class Solution {
- 
+
+    class Tuple{
+        TreeNode node;
+        int x;
+        int y;
+        public Tuple(TreeNode node, int x, int y){
+            this.node = node;
+            this.x = x;
+            this.y = y;
+        }
+    }
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-
-
-        List<List<Integer>> result =  new ArrayList<>();
-        Queue<Tuple> queue = new LinkedList<>();
+        TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> result =  new TreeMap<>();
+        Queue<Tuple> queue =  new LinkedList<>();
         queue.offer(new Tuple(root,0,0));
-        TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> map = new  TreeMap<>();
 
         while(!queue.isEmpty()){
 
-            Tuple tuple = queue.remove();
-            int x = tuple.row;
-            int y = tuple.col;
+            Tuple removedTuple = queue.poll();
+            int x = removedTuple.x;
+            int y = removedTuple.y;
 
-            if(!map.containsKey(x)){
-                map.put(x,new TreeMap<>());
+            if(!result.containsKey(x)){
+                result.put(x,new TreeMap<>());
             }
 
-            if(!map.get(x).containsKey(y)){
-                map.get(x).put(y,new PriorityQueue<>());
-            }
-            map.get(x).get(y).add(tuple.node.val);
-
-
-            if(tuple.node.left!=null){
-                queue.offer(new Tuple(tuple.node.left,x-1,y+1));
+            if(!result.get(x).containsKey(y)){
+                result.get(x).put(y,new PriorityQueue<>());
             }
 
-            if(tuple.node.right!=null){
-                queue.offer(new Tuple(tuple.node.right,x+1,y+1));
+            result.get(x).get(y).add(removedTuple.node.val);
+
+            if(removedTuple.node.left!=null){
+                queue.offer(new Tuple(removedTuple.node.left,x-1,y+1));
             }
 
+            if(removedTuple.node.right!=null){
+                queue.offer(new Tuple(removedTuple.node.right,x+1,y+1));
+            }
         }
-        List<List<Integer>> resList = new ArrayList<>();
-        for(TreeMap<Integer,PriorityQueue<Integer>> entry : map.values()){
 
-        List<Integer> tempList = new ArrayList<>();
+        System.out.println(Arrays.toString(result.entrySet().toArray()));
 
-        for(PriorityQueue<Integer> priorityQueue : entry.values()){
-                while(!priorityQueue.isEmpty()){
-                    tempList.add(priorityQueue.remove());
+
+        List<List<Integer>> resultList = new ArrayList<>();
+
+        for(TreeMap<Integer,PriorityQueue<Integer>> mapValue: result.values()){
+            List<Integer> tempList = new ArrayList<>();
+            for(PriorityQueue<Integer> queueValue : mapValue.values()){
+                while(!queueValue.isEmpty()){
+                    tempList.add(queueValue.poll());
                 }
             }
-            resList.add(tempList);
+            resultList.add(tempList);
         }
 
-         return resList;
-
-
-        
+        return resultList;
     }
 }

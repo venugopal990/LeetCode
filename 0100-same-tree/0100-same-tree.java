bff10d1ec0b4215a5876907f6ac526aff0e0e09f
Venu Gopal Dummu <venugopal.dummu@gmail.com>
2024-05-14 11:20:44 -0500

    Time: 0 ms (100.00%), Space: 41.2 MB (16.03%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -16,22 +16,9 @@
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
 
-
-        if(p!=null && q!=null &&  p.val != q.val){
-            return false;
-        }else if(p!=null && q!=null &&  p.val == q.val){
-            if(isSameTree(p.left, q.left) &&  isSameTree(p.right, q.right)){
-                return true;
-            }else{
-                return false;
-            }
-        }else if(p!=null && q==null){
-            return false;
-        }else if(q!=null && p==null){
-            return false;
+        if (p == null || q == null) {
+            return (p == q);
         }
-        
-       return true;
-
+        return (p.val == q.val) && isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
     }
 }

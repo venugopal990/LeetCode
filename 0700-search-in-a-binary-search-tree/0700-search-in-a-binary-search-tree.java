7d76bb7a50994e096fb98967fc4fd012a73dc693
Venu Gopal Dummu <venugopal.dummu@gmail.com>
2024-05-18 15:26:31 -0500

    Time: 0 ms (100.00%), Space: 45.2 MB (47.13%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
@@ -15,18 +15,16 @@
  */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-
         if(root == null){
             return null;
         }
 
-        if(root.val == val){
-            return root;
-        }else if(val>root.val){
+        if(val < root.val){
+            return searchBST(root.left,val);
+        }else if(val > root.val){
             return searchBST(root.right,val);
         }else{
-            return searchBST(root.left,val);
+            return root;
         }
-
     }
 }

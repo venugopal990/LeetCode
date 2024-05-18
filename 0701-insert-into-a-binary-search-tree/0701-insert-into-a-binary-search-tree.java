fa2bdd7230a3d59ccef630feb167bf52a7a90ee4
Venu Gopal Dummu <venugopal.dummu@gmail.com>
2024-05-18 16:06:24 -0500

    Time: 0 ms (100.00%), Space: 44.8 MB (85.40%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
@@ -15,18 +15,16 @@
  */
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
-        
         if(root == null){
-           return new TreeNode(val);
+            return new TreeNode(val);
         }
 
-    
-        if(val > root.val){
-            root.right = insertIntoBST(root.right,val);
-        }else {
+        if(val<root.val){
             root.left = insertIntoBST(root.left,val);
+        }else if(val>root.val){
+            root.right = insertIntoBST(root.right,val);
         }
+
         return root;
-        
     }
 }

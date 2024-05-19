ddeaf75256c1700d6df261a7fb22551650a4639b
Venu Gopal Dummu <venugopal.dummu@gmail.com>
2024-05-19 02:15:18 -0500

    Time: 0 ms (100.00%), Space: 45 MB (5.04%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -1,45 +1,41 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-
     int count = 0;
-    int min  = 0;
-
+    int result = Integer.MIN_VALUE;
     public int kthSmallest(TreeNode root, int k) {
-
-        count = k;
-        inOrder(root);
-        return min;
-
+        inOrder(root,k);
+        return result;
     }
 
-    private void inOrder(TreeNode root) {
 
-        if (root == null) {
+    private void inOrder(TreeNode root, int k){
+        if(root == null){
             return;
         }
 
-        inOrder(root.left);
-        if (count > 0) {
-            min = root.val;
-            count--;
-        } else {
+        kthSmallest(root.left, k);
+
+        count++;
+        if(k == count){
+            result = root.val; 
             return;
         }
-        inOrder(root.right);
+        kthSmallest(root.right, k);
+       
 
     }
 }

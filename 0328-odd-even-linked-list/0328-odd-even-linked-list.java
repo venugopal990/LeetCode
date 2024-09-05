c5f0a66dd892a98ab61c0310074bac6c5a6cc446
Venu Gopal Dummu <venugopal.dummu@gmail.com>
2024-09-04 23:06:38 -0500

    Time: 0 ms (100.00%), Space: 44.1 MB (91.99%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -15,22 +15,22 @@ class Solution {
             return null;
         }
 
-        ListNode odd =  head;
+        ListNode odd = head;
         ListNode even =  head.next;
 
         ListNode evenHead = even;
+
         while(even!=null && even.next!=null){
-            odd.next = odd.next.next;
+            odd.next =  odd.next.next;
             odd = odd.next;
 
             even.next =  even.next.next;
-            even= even.next;
-        }
+            even  = even.next;
+        }  
 
         odd.next = evenHead;
 
-
         return head;
         
-    }  
+    }
 }
